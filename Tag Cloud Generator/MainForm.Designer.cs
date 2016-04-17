4be888d1e89e906fbffc8367cf2082b759ef8f41
nikit <nikitosbobin@gmail.com>
2016-04-17 20:43:43 +0500

    Добавил превью для readme

--- Tag Cloud Generator/MainForm.Designer.cs
+++ Tag Cloud Generator/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.textLoadGroup = new System.Windows.Forms.GroupBox();
             this.loadedFilePath = new System.Windows.Forms.TextBox();
             this.fileLabel = new System.Windows.Forms.Label();
@@ -519,6 +520,7 @@
             this.Controls.Add(this.cloudGeneratingGroup);
             this.Controls.Add(this.textLoadGroup);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(744, 548);
             this.Name = "MainForm";
             this.Text = "Tag cloud generator";
